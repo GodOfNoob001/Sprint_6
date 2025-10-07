f408a18d34d3545c678a47baf551d813731fd4df
Alex Dmiterko <dmiterko.sascha5@gmail.com>
2025-10-07 20:15:47 +0300

    third commit

---
+++ conftest.py
@@ -0,0 +1,35 @@
+import pytest
+import allure
+from selenium import webdriver
+from urls import YandexScooterUrls
+from pages.main_page import MainPage
+
+
+@pytest.fixture(scope="function")
+def driver():
+    with allure.step('Создаем новый клиент браузера для совершения тестовых прогонов'):
+        driver_instance = webdriver.Firefox()
+        driver_instance.get(YandexScooterUrls.base_url)
+        main_page = MainPage(driver_instance)
+        main_page.close_cookie_banner()
+
+    yield driver_instance
+
+    with allure.step('Закрываем созданный экземпляр браузерного клиента'):
+        driver_instance.quit()
+
+@pytest.fixture
+def main_page(driver):
+    return MainPage(driver)
+
+
+@pytest.fixture
+def order_page(driver):
+    from pages.page_to_make_order import OrderPage
+    return OrderPage(driver)
+
+
+@pytest.fixture
+def base_page(driver):
+    from pages.base_page import BasePage
+    return BasePage(driver)
--- pages/page_to_make_order.py
+++ pages/page_to_make_order.py
@@ -1,84 +1,63 @@
+import allure
 from selenium.webdriver import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
 from locators.first_page_to_make_order_locators import LocatorsFirstPageOrder
 from selenium.webdriver.common.by import By
 from locators.second_page_to_make_order_locators import LocatorsSecondPageOrder
 from locators.screen_to_confirm_order_locators import LocatorsConfirmationOrderScreen
 from locators.screen_success_confirmation_locators import LocatorsSuccessOrderScreen
+from pages.base_page import BasePage
 
-
-class OrderPage:
+class OrderPage(BasePage):
     def __init__(self, driver):
-        self.driver = driver
-
+        super().__init__(driver)
+    @allure.step('Заполнить первую страницу заказа')
     def fill_first_page(self, order_data):
-        self.driver.find_element(*LocatorsFirstPageOrder.NAME_INPUT_LOCATOR).send_keys(order_data["name"])
-        self.driver.find_element(*LocatorsFirstPageOrder.SECOND_NAME_INPUT_LOCATOR).send_keys(order_data["surname"])
-        self.driver.find_element(*LocatorsFirstPageOrder.ADDRESS_INPUT_LOCATOR).send_keys(order_data["address"])
+        self.send_keys_to_element(LocatorsFirstPageOrder.NAME_INPUT_LOCATOR, order_data["name"])
+        self.send_keys_to_element(LocatorsFirstPageOrder.SECOND_NAME_INPUT_LOCATOR, order_data["surname"])
+        self.send_keys_to_element(LocatorsFirstPageOrder.ADDRESS_INPUT_LOCATOR, order_data["address"])
         self.select_metro_station(order_data["metro"])
-        self.driver.find_element(*LocatorsFirstPageOrder.PHONE_INPUT_LOCATOR).send_keys(order_data["phone"])
-        self.driver.find_element(*LocatorsFirstPageOrder.NEXT_PAGE_BUTTON).click()
+        self.send_keys_to_element(LocatorsFirstPageOrder.PHONE_INPUT_LOCATOR, order_data["phone"])
+        self.click(LocatorsFirstPageOrder.NEXT_PAGE_BUTTON)
 
+    @allure.step('Выбрать станцию метро')
     def select_metro_station(self, station_name):
-        metro_input = self.driver.find_element(*LocatorsFirstPageOrder.METRO_INPUT_LOCATOR)
-        metro_input.click()
-        metro_input.send_keys(station_name)
-        station_option = WebDriverWait(self.driver, 5).until(
-            expected_conditions.element_to_be_clickable((By.XPATH, f"//div[text()='{station_name}']"))
-        )
-        station_option.click()
+        self.click(LocatorsFirstPageOrder.METRO_INPUT_LOCATOR)
+        self.send_keys_to_element(LocatorsFirstPageOrder.METRO_INPUT_LOCATOR, station_name)
+        station_locator = (By.XPATH, f"//div[text()='{station_name}']")
+        self.click(station_locator)
 
+    @allure.step('Заполнить вторую страницу заказа')
     def fill_second_page(self, order_data):
-        date_input = self.driver.find_element(*LocatorsSecondPageOrder.DATE_OF_DELIVERY_INPUT)
+        date_input = self.find_element(LocatorsSecondPageOrder.DATE_OF_DELIVERY_INPUT)
         date_input.send_keys(order_data["date"])
         date_input.send_keys(Keys.ENTER)
         self.select_rental_period(order_data["rental_period"])
         self.select_scooter_color(order_data["color"])
-        comment_input = self.driver.find_element(*LocatorsSecondPageOrder.COMMENT_FOR_DELIVERER)
-        comment_input.send_keys(order_data["comment"])
-        self.driver.find_element(*LocatorsSecondPageOrder.ORDER_BUTTON).click()
+        self.send_keys_to_element(LocatorsSecondPageOrder.COMMENT_FOR_DELIVERER, order_data["comment"])
+        self.click(LocatorsSecondPageOrder.ORDER_BUTTON)
 
-
+    @allure.step('Выбрать период аренды')
     def select_rental_period(self, period):
-        dropdown = WebDriverWait(self.driver, 10).until(
-            expected_conditions.element_to_be_clickable(LocatorsSecondPageOrder.RENTAL_PERIOD_DROPDOWN)
-        )
-        dropdown.click()
-        WebDriverWait(self.driver, 10).until(
-            expected_conditions.visibility_of_element_located(LocatorsSecondPageOrder.RENTAL_PERIOD_DROPDOWN_MENU)
-        )
+        self.click(LocatorsSecondPageOrder.RENTAL_PERIOD_DROPDOWN)
         period_locators = {
             "сутки": LocatorsSecondPageOrder.RENTAL_PERIOD_1_DAY,
             "семеро суток": LocatorsSecondPageOrder.RENTAL_PERIOD_7_DAYS
         }
+        self.click(period_locators[period])
 
-        period_element = WebDriverWait(self.driver, 10).until(
-            expected_conditions.element_to_be_clickable(period_locators[period])
-        )
-        period_element.click()
-
+    @allure.step('Выбрать цвет самоката')
     def select_scooter_color(self, color):
         color_locators = {
             "black": LocatorsSecondPageOrder.BLACK_COLOR_CHECKBOX,
             "grey": LocatorsSecondPageOrder.GREY_COLOR_CHECKBOX
         }
+        self.click(color_locators[color])
 
-        color_checkbox = self.driver.find_element(*color_locators[color])
-        color_checkbox.click()
-
+    @allure.step('Подтвердить заказ')
     def confirm_order(self):
-        confirm_button = self.driver.find_element(*LocatorsConfirmationOrderScreen.YES_BUTTON)
-        confirm_button.click()
+        self.click(LocatorsConfirmationOrderScreen.YES_BUTTON)
 
+    @allure.step('Проверить отображение сообщения об успехе')
     def check_success_message(self):
-        success_message = WebDriverWait(self.driver, 10).until(
-            expected_conditions.visibility_of_element_located(LocatorsSuccessOrderScreen.SUCCESS_MESSAGE)
-        )
-        success_button = WebDriverWait(self.driver, 10).until(
-            expected_conditions.visibility_of_element_located(LocatorsSuccessOrderScreen.CHECK_ORDER_BUTTON)
-        )
-        if success_message.is_displayed() and success_button.is_displayed():
-            return True
-        else:
-            return False
+        return (self.is_element_visible(LocatorsSuccessOrderScreen.SUCCESS_MESSAGE) and
+                self.is_element_visible(LocatorsSuccessOrderScreen.CHECK_ORDER_BUTTON))
