1fca6534a1b14889e0fbbb00f22eb3b7e72a5686
Alex Dmiterko <dmiterko.sascha5@gmail.com>
2025-10-07 20:10:23 +0300

    добавлено наследование

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,7 +5,7 @@ from pages.base_page import BasePage
 
 class MainPage(BasePage):
     def __init__(self, driver):
-        self.driver = driver
+        super().__init__(driver)
 
     def close_cookie_banner(self):
         cookie_button = WebDriverWait(self.driver, 5).until(expected_conditions.element_to_be_clickable(LocatorsBasePage.COOKIE_BUTTON))
